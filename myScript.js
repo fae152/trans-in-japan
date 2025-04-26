d47b9c163918dd223207f2ba7b9fe43867f6e8e1
smit152 <sarahmichiru@gmail.com>
2025-04-26 12:49:13 -0700

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,52 +1,109 @@
-function button() {
-  let x = document.getElementById("contact")
-  switch (x) {
-  case contact:
-    x.style.display = "block";
-  case resources:
-    x.style.display = "none";
-  default:
-    x.style.display = "none";
-  }
-}
+let names = {
+"camera": "Progress",
+"nook-miles": "miles",
+"critterpedia": "Critterpedia",
+"diy": "Weaving",
+"design": "design",
+"map": "Map",
+"chat": "Contact",
+"passport": "About",
+"best-friends": "Resources",
+"island-life": "Blog",
+"instagram": "Instagram",
+"youtube":"YouTube",
+"spotify":"Podcast",
+};
 
-function resourcesButton() {
-  var x = document.getElementById("resources");
+$(".app-icon").on({
+    mouseenter: function () {
+        $(".title").text(names[$(this).attr("id")]);
+    },
+    mouseleave: function () {
+        $(".title").text("Trans in Japan");
+    }
+});
+$(".app-icon-insta").on({
+  mouseenter: function () {
+      $(".title").text(names[$(this).attr("id")]);
+  },
+  mouseleave: function () {
+      $(".title").text("Trans in Japan");
+  }
+});
+
+function updateTime() {
+    let date = new Date();
+    $('.time').text(date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }));
+}
+updateTime();
+setInterval(updateTime, 500);
+
+/*Get current year for copyright statement*/
+$('#spanYear').html(new Date().getFullYear());
+
+/*Show Contact Form onClick*/
+function contactForm() {
+  var x = document.getElementById("contact-form");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
 }
-  let names = {
-  "camera": "Instagram",
-  "nook-miles": "Blog",
-  "critterpedia": "Critterpedia",
-  "diy": "Resources",
-  "design": "Progress",
-  "map": "Map",
-  "chat": "Contact",
-  "passport": "About",
-  "best-friends": "Best Friends List",
-  "youtube":"YouTube",
-  "spotify":"Podcast",
-  };
 
-  $(".app-icon").on({
-      mouseenter: function () {
-          $(".title").text(names[$(this).attr("id")]);
-      },
-      mouseleave: function () {
-          $(".title").text("Trans in Japan");
-      }
-  });
 
-  function updateTime() {
-      let date = new Date();
-      $('.time').text(date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }));
+/*Show new page onClick*/
+/*let p = document.getElementById("progress");
+  let r = document.getElementById("resources");
+  let c = document.getElementById("contact"); 
+function designButton() {
+   
+  if (p.style.display === "none") {
+    p.style.display = "block";
+    c.style.display = "none";
+    r.style.display = "none";
+  } else if (p.style.display === "none" && c.style.display === "none" && r.style.display === "block") {
+    p.style.display = "block";
+    c.style.display = "none";
+    r.style.display =="none";
+  } else {
+    p.style.display = "none";
+    c.style.display = "none";
+    r.style.display = "none";
   }
-  updateTime();
-  setInterval(updateTime, 500);
+}
 
-{/*Get current year for copyright statement*/}
-$('#spanYear').html(new Date().getFullYear());
+function diyButton() {
+
+
+  if (r.style.display === "none") {
+    r.style.display = "block";
+    p.style.display = "none";
+    c.style.display = "none";
+  } else if (r.style.display === "none" && c.style.display === "none" && p.style.display === "block") {
+    r.style.display = "block";
+    p.style.display = "none";
+    c.style.display = "none";
+  } else {
+    r.style.display = "none";
+    p.style.display = "none";
+    c.style.display = "none";
+  }
+}
+
+function chatButton() {
+
+  if (c.style.display === "none") {
+    c.style.display = "block";
+    r.style.display = "none";
+    p.style.display = "none";
+  } else if (c.style.display === "none" && r.style.display === "block" && p.style.display === "none") {
+    c.style.display = "block";
+    r.style.display = "none";
+    p.style.display = "none";
+  } else {
+    c.style.display = "none";
+    r.style.display = "none";
+    p.style.display = "none";
+  }
+}*/
